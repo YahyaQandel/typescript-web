45204a7fa161a3f37e8307ba888281df9c5d60dd
YahyaQandel <yahya.qandel@gmail.com>
2023-05-20 22:54:07 +0300

    rename spec file, add specPattern

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    specPattern: 'cypress/e2e/**/*.spec.ts',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
